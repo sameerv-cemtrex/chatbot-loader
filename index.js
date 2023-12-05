8cb47c64c14abd64b3e7301be275f0bced248f0d
sameerv-cemtrex <svanjari@cemtrexlabs.com>
2023-12-05 19:03:52 +0530

    style: styling of the float button and testing to see the position

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
-(function(){
-    
-    const content = document.getElementById("message");
-    const send = document.getElementById("send");
-    const hide =  document.getElementById("hide");
-    const show =  document.getElementById("show");
-    const toggle =  document.getElementById("toggle");
-    
-    const changeButtonsState = () => {
-        show.disabled = !show.disabled;
-        hide.disabled = !show.disabled;
-    } 
-    
-    widgetApi().then( api => {
-        
-        hide.disabled = false;
-        toggle.disabled = false;
-        
-        send.addEventListener("click", () => {
-            const value = content.value;
-            if ( value.length > 0 ) {
-                api.sendMessage(value);
-                content.value = "";
-            }
-        });
-        
-        hide.addEventListener("click", () => {
-            changeButtonsState();
-            api.hide();
-        });
+(function () {
+  const content = document.getElementById("message");
+  const send = document.getElementById("send");
+  const hide = document.getElementById("hide");
+  const show = document.getElementById("show");
+  const toggle = document.getElementById("toggle");
 
-        show.addEventListener("click", () => {
-            changeButtonsState();
-            api.show();
-        });
+  const changeButtonsState = () => {
+    show.disabled = !show.disabled;
+    hide.disabled = !show.disabled;
+  };
 
-        toggle.addEventListener("click", () => {
-            changeButtonsState();
-            api.toggle();
-        });
-        
-        api.onHide = () => changeButtonsState();
-        
+  widgetApi().then((api) => {
+    hide.disabled = false;
+    toggle.disabled = false;
+
+    // send.addEventListener("click", () => {
+    //     const value = content.value;
+    //     if ( value.length > 0 ) {
+    //         api.sendMessage(value);
+    //         content.value = "";
+    //     }
+    // });
+
+    hide.addEventListener("click", () => {
+      changeButtonsState();
+      api.hide();
     });
-    
+
+    show.addEventListener("click", () => {
+      changeButtonsState();
+      api.show();
+    });
+
+    toggle.addEventListener("click", () => {
+      changeButtonsState();
+      api.toggle();
+    });
+
+    api.onHide = () => changeButtonsState();
+  });
 })();
--- loader.js
+++ loader.js
@@ -1,106 +1,112 @@
 (() => {
-    
-    const script = document.currentScript;
-    
-     const loadWidget = () => {
-        
-        const widget= document.createElement("div");
+  const script = document.currentScript;
 
-        const widgetStyle = widget.style;
-        widgetStyle.display = "none";
-        widgetStyle.boxSizing = "border-box";
-        widgetStyle.width = "400px";
-        widgetStyle.height = "647px";
-        widgetStyle.position = "absolute";
-        widgetStyle.top = "40px";
-        widgetStyle.right = "40px";
+  const loadWidget = () => {
+    const widget = document.createElement("div");
+    const floatBtn = document.createElement("div");
 
-        const iframe = document.createElement("iframe");
-        
-        const iframeStyle = iframe.style;
-        iframeStyle.boxSizing = "borderBox";
-        iframeStyle.position = "absolute";
-        iframeStyle.right = 0;
-        iframeStyle.top = 0;
-        iframeStyle.width = "100%";
-        iframeStyle.height = "100%";
-        iframeStyle.border = 0;
-        iframeStyle.margin = 0;
-        iframeStyle.padding = 0;
-        iframeStyle.width = "500px";
+    const floatBtnStyle = floatBtn.style;
+    floatBtnStyle.position = "absolute";
+    floatBtnStyle.bottom = "30px";
+    floatBtnStyle.right = "30px";
+    floatBtnStyle.zIndex = 123654;
+    floatBtnStyle.width = "50px";
+    floatBtnStyle.height = "50px";
+    floatBtnStyle.borderRadius = "25px";
+    floatBtnStyle.background = "red";
 
-        widget.appendChild(iframe);
+    const widgetStyle = widget.style;
+    widgetStyle.display = "none";
+    widgetStyle.boxSizing = "border-box";
+    widgetStyle.width = "350px";
+    widgetStyle.height = "450px";
+    widgetStyle.position = "absolute";
+    widgetStyle.bottom = "35px";
+    widgetStyle.right = "20px";
 
-        const greeting = script.getAttribute("data-greeting");
+    const iframe = document.createElement("iframe");
 
-        const api = {
-            
-            sendMessage: (message) => {
-                iframe.contentWindow.postMessage({
-                    sendMessage:message
-                }, "http://localhost:3000");
-            },
-            
-            show: () => {
-                widget.style.display = "block";
-            },
-            
-            hide: () => {
-                widget.style.display = "none";
-            },
-            
-            toggle: () => {
-                const display = window.getComputedStyle(widget, null).display;
-                widget.style.display = display === "none" ? "block" : "none"; 
-            },
-            
-            onHide: () => {}
-            
-        }
-        
-        iframe.addEventListener("load", () => {
-            
-            window.addEventListener("getWidgetApi", () => {
-                
-                const event = new CustomEvent('widgetApi', {detail: api});
-                window.dispatchEvent(event);
-                
-            });
-            
-            window.addEventListener("message", evt => {
-                
-                if( evt.origin !== "http://localhost:3000") {
-                    return;
-                }
-                
-                if (evt.data === "hide") {
-                    api.hide();
-                    api.onHide();
-                }
-                
-            });
-            
-            iframe.contentWindow.postMessage({greeting}, "http://localhost:3000");
-            widgetStyle.display = "block";
-        });
-        
-        const license = script.getAttribute("data-license");
-        const widgetUrl = `http://localhost:3000?license=${license}`;
-        
-        iframe.src = widgetUrl;
+    const iframeStyle = iframe.style;
+    iframeStyle.boxSizing = "borderBox";
+    iframeStyle.position = "absolute";
+    iframeStyle.right = 0;
+    iframeStyle.top = 0;
+    iframeStyle.width = "100%";
+    iframeStyle.height = "100%";
+    iframeStyle.border = 0;
+    iframeStyle.borderRadius = "0.5rem";
+    iframeStyle.margin = 0;
+    iframeStyle.padding = 0;
+    iframeStyle.width = "350px";
 
-        document.body.appendChild(widget);
-        
-    }
+    widget.appendChild(iframe);
+    document.body.appendChild(floatBtn);
+    window.console.log("hey");
 
-    if ( document.readyState === "complete" ) {
+    const greeting = script.getAttribute("data-greeting");
+
+    const api = {
+      sendMessage: (message) => {
+        iframe.contentWindow.postMessage(
+          {
+            sendMessage: message,
+          },
+          "http://localhost:3000"
+        );
+      },
+
+      show: () => {
+        widget.style.display = "block";
+      },
+
+      hide: () => {
+        widget.style.display = "none";
+      },
+
+      toggle: () => {
+        const display = window.getComputedStyle(widget, null).display;
+        widget.style.display = display === "none" ? "block" : "none";
+      },
+
+      onHide: () => {},
+    };
+
+    iframe.addEventListener("load", () => {
+      window.addEventListener("getWidgetApi", () => {
+        const event = new CustomEvent("widgetApi", { detail: api });
+        window.dispatchEvent(event);
+      });
+
+      // window.addEventListener("message", (evt) => {
+      //   if (evt.origin !== "http://localhost:3000/chat") {
+      //     return;
+      //   }
+
+      //   if (evt.data === "hide") {
+      //     api.hide();
+      //     api.onHide();
+      //   }
+      // });
+
+      iframe.contentWindow.postMessage({ greeting }, "http://localhost:3000");
+      widgetStyle.display = "block";
+    });
+
+    const license = script.getAttribute("data-license");
+    const widgetUrl = `http://localhost:3000/chat`;
+
+    iframe.src = widgetUrl;
+
+    document.body.appendChild(widget);
+  };
+
+  if (document.readyState === "complete") {
+    loadWidget();
+  } else {
+    document.addEventListener("readystatechange", () => {
+      if (document.readyState === "complete") {
         loadWidget();
-    } else {
-        document.addEventListener("readystatechange", () => {
-            if ( document.readyState === "complete" ) {
-                loadWidget();
-            }
-        });
-    }
-
+      }
+    });
+  }
 })();
