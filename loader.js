c4f3fdba488f5eee8a89274ce41793e1260a8d4f
sameerv-cemtrex <svanjari@cemtrexlabs.com>
2023-12-08 15:44:53 +0530

    style: added svg image to float btn

--- loader.js
+++ loader.js
@@ -14,6 +14,9 @@
     floatBtnStyle.height = "40px";
     floatBtnStyle.borderRadius = "25px";
     floatBtnStyle.background = "#0f0";
+    floatBtnStyle.display = "grid";
+    floatBtnStyle.placeItems = "center";
+    floatBtnStyle.cursor = "pointer";
 
     const widgetStyle = widget.style;
     widgetStyle.display = "none";
@@ -23,6 +26,9 @@
     widgetStyle.position = "fixed";
     widgetStyle.bottom = "65px";
     widgetStyle.right = "35px";
+
+    const svgData = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-message-square"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/></svg>`;
+    floatBtn.innerHTML = svgData;
 
     const iframe = document.createElement("iframe");
 
@@ -55,7 +61,7 @@
     iframe.addEventListener("load", () => {});
 
     const license = script.getAttribute("data-license");
-    const widgetUrl = `http://localhost:3000/chat`;
+    const widgetUrl = `https://theckdev.com/chat`;
 
     iframe.src = widgetUrl;
 
